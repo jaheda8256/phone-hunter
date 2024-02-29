34fa0cd745cddac364efb8c569d173ef1d7fb169
jaheda8256 <jahedasultana47@gmail.com>
2024-02-29 07:55:52 +0600

    added hidden spinner

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async() =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async(searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -7,14 +7,27 @@ const loadPhone = async() =>{
 } 
 
 const displayPhones = phones =>{
-    // console.log(phones)
+    console.log(phones)
     const phoneContainer = document.getElementById('phone-container');
+// clear phone container cards  before adding new cards 
+phoneContainer.textContent = '';
+const showAllContainer = document.getElementById('show-all-container');
+if(phones.length > 12){
+    showAllContainer.classList.remove('hidden');
+}
+else{
+    showAllContainer.classList.add('hidden');
+}
+
+// display only first 12 phones
+phones = phones.slice(0,12);
+
 
     phones.forEach(phone => {
         console.log(phone);
         // 2 create a phone
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
         // 3 set innerText HTML
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -25,11 +38,36 @@ const displayPhones = phones =>{
         <button class="btn btn-primary">Buy Now</button>
         </div>
         </div>
-
         `;
+
         // 4 append child
         phoneContainer.appendChild(phoneCard);
-    })
+    });
+
+    toggleLoadingSpinner(false);
 }
 
-loadPhone()
+const handleSearch = () =>{
+    toggleLoadingSpinner(true);
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText);
+}
+
+const handleSearch2 = () =>{
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field2');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+}
+const toggleLoadingSpinner = (isLoading) =>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+}
+// loadPhone()
